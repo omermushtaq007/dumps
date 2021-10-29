a1e991859fa677552710ec40aac0f9720a8892fe
umer mushtaq <umermushtaq3424@gmail.com>
2021-10-29 23:44:11 +0500

    mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 const app = express(); // entire app
 const port = process.env.PORT || 8888;
-app.listen(port, () => {
-  console.log(`Server started on ${port}`);
-});
+// cors middleware init
+app.use(cors());
+app.use(
+  express.json({
+    extended: false,
+  }),
+);
+app.use('/api/v2/test', (req, res) => res.json('hally hally mall dia'));
+// invalid req or api handler.
+app.use('*', (req, res) => res.json('Invalid Api Address').status(404));
+
+app.listen(port, () => console.log(`Server started on ${port}`));
