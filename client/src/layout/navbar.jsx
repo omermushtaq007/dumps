69106ab323eba3714f996533b05a271450d01edd
umer mushtaq <umermushtaq3424@gmail.com>
2022-01-12 19:52:54 +0500

    client deleted

--- client/src/layout/navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Navbar() {
-    return (
-        <div>
-            <button disabled="disabled" className='btn btn-danger'></button>
-        </div>
-    )
-}
